1dc5933d6be1bf0a7f90f345ef5511272fe1e110
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-01-04 19:28:56 +0900

    Create 32-A.rb
    http://arc032.contest.atcoder.jp/tasks/arc032_1
    http://arc032.contest.atcoder.jp/submissions/314554

---
+++ Regular/32-A.rb
@@ -0,0 +1,14 @@
+#AtCorder Regular 32 A
+require 'prime'
+input =  STDIN.gets.chomp
+sum = 0
+n = input.to_i
+n.downto(0){|n|
+	sum += n
+}
+
+if sum.prime?
+	puts "WANWAN"
+else
+	puts "BOWWOW"
+end
