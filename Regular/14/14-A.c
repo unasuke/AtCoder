b67d6937101082794e6f72a9eda675d301773e53
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-02-17 00:00:37 +0900

    Create Regular 14-A.c
    http://arc014.contest.atcoder.jp/tasks/arc014_1
    http://arc014.contest.atcoder.jp/submissions/339080

---
+++ Regular/14/14-A.c
@@ -0,0 +1,15 @@
+//AtCoder Regular 14 A
+#include <stdio.h>
+
+int main( void ){
+  int n;
+  scanf( "%d" , &n );
+  if( n % 2 == 1){
+    puts( "Red" );
+  }
+  else{
+    puts( "Blue" );
+  }
+
+  return 0;
+}
