c3d36ec77923d4fe8109f0afb247daccd7889630
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-02-19 09:14:12 +0900

    Create Regular 15-A.c
    http://arc015.contest.atcoder.jp/tasks/arc015_1
    http://arc015.contest.atcoder.jp/submissions/339734

---
+++ Regular/15/15-A.c
@@ -0,0 +1,10 @@
+//AtCoder Regular 15 A
+#include <stdio.h>
+int main( void ){
+  double celsius;
+
+  scanf( "%lf" , &celsius );
+  printf( "%lf\n" , ( (9.0 / 5.0) * celsius ) + 32.0 );
+
+  return 0;
+}
