5ebea165b4cf7884af471a4e447c9f350b0f3627
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-02-24 21:24:17 +0900

    Create Beginner 13-A.rb
    http://abc013.contest.atcoder.jp/tasks/abc013_1
    http://abc013.contest.atcoder.jp/submissions/343631

---
+++ Beginner/13/13-A.rb
@@ -0,0 +1,13 @@
+#AtCoder Beginner 13 A
+input = STDIN.gets.chomp
+if input == 'A' then
+  puts '1'
+elsif input == 'B' then
+  puts '2'
+elsif input == 'C' then
+  puts '3'
+elsif input == 'D' then
+  puts '4'
+elsif input == 'E' then
+  puts '5'
+end
