f406ffd3389720bb5b244c796b358fbbcf533c12
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-03-18 01:22:02 +0900

    Create Beginner 13-A.py
    http://abc013.contest.atcoder.jp/tasks/abc013_1
    http://abc013.contest.atcoder.jp/submissions/359527

---
+++ Beginner/13/13-A.py
@@ -0,0 +1,12 @@
+#AtCoder Beginner 13 A
+str = raw_input()
+if str == 'A':
+    print "1"
+elif str == 'B':
+    print "2"
+elif str == 'C':
+    print "3"
+elif str == 'D':
+    print "4"
+elif str == 'E':
+    print "5"
