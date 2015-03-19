84d28e3641ed77787ca880d590e2fc2403b8ee2b
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-03-19 20:59:46 +0900

    Create Regular 12-A.py
    http://arc012.contest.atcoder.jp/tasks/arc012_1
    http://arc012.contest.atcoder.jp/submissions/362983

---
+++ Regular/12/12-A.py
@@ -0,0 +1,14 @@
+#AtCoder Beginner 13 A
+str = raw_input()
+if str == "Sunday" or str == "Saturday":
+    print "0"
+elif str == "Monday":
+    print "5"
+elif str == "Tuesday":
+    print "4"
+elif str == "Wednesday":
+    print "3"
+elif str == "Thursday":
+    print "2"
+elif str == "Friday":
+    print "1"
